0e2da8cb25b5de06ebf74f9f8e5f401ad6840976
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-12-19 10:56:20 +0100

    API versions.

--- CitiesManagerSolution/CitiesManager.WebAPI/Program.cs
+++ CitiesManagerSolution/CitiesManager.WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using CitiesManager.WebAPI.DatabaseContext;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,18 +13,47 @@ builder.Services.AddControllers(options => {
 })
  .AddXmlSerializerFormatters();
 
+
+//Enable versioning in Web API controllers
+builder.Services.AddApiVersioning(config =>
+{
+ config.ApiVersionReader = new UrlSegmentApiVersionReader(); //Reads version number from request url at "apiVersion" constraint
+
+ //config.ApiVersionReader = new QueryStringApiVersionReader(); //Reads version number from request query string called "api-version". Eg: api-version=1.0
+
+ //config.ApiVersionReader = new HeaderApiVersionReader("api-version"); //Reads version number from request header called "api-version". Eg: api-version: 1.0
+
+ config.DefaultApiVersion = new ApiVersion(1, 0);
+ config.AssumeDefaultVersionWhenUnspecified = true;
+});
+
+
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
  options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
 
+
 //Swagger
 builder.Services.AddEndpointsApiExplorer(); //Generates description for all endpoints
+
+
 builder.Services.AddSwaggerGen(options => {
  options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "api.xml"));
 
+ options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Cities Web API", Version = "1.0" });
+
+ options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Cities Web API", Version = "2.0" });
+
 }); //generates OpenAPI specification
 
+
+builder.Services.AddVersionedApiExplorer(options => {
+ options.GroupNameFormat = "'v'VVV"; //v1
+ options.SubstituteApiVersionInUrl = true;
+});
 
 var app = builder.Build();
 
@@ -33,7 +63,11 @@ app.UseHsts();
 app.UseHttpsRedirection();
 
 app.UseSwagger(); //creates endpoint for swagger.json
-app.UseSwaggerUI(); //creates swagger UI for testing all Web API endpoints / action methods
+app.UseSwaggerUI(options  =>
+{
+ options.SwaggerEndpoint("/swagger/v1/swagger.json", "1.0");
+ options.SwaggerEndpoint("/swagger/v2/swagger.json", "2.0");
+}); //creates swagger UI for testing all Web API endpoints / action methods
 
 app.UseAuthorization();
 
