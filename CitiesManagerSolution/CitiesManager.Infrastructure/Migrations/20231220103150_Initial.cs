166a886148dcdc2d6fa3956e1c29ab8111a0f099
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-12-20 11:36:48 +0100

    Clean Architecture implementation.

--- CitiesManagerSolution/CitiesManager.Infrastructure/Migrations/20231220103150_Initial.cs
+++ CitiesManagerSolution/CitiesManager.Infrastructure/Migrations/20231220103150_Initial.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace CitiesManager.WebAPI.Migrations.SqlServerMigrations
+namespace CitiesManager.Infrastructure.Migrations
 {
     /// <inheritdoc />
     public partial class Initial : Migration
